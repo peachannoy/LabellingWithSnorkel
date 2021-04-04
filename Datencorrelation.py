edde10255a32cb1d2fd191e3d834a5cab4bb1bab
Reepham <timo.hannemann@hani-web.de>
2021-04-04 14:14:13 +0200

    leichte Namensänderung bei Datenvorbereitung -Timo

--- Datencorrelation.py
+++ Datencorrelation.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 # data["RainTomorrow"]= data["RainTomorrow"].map({"Yes":1,"No":0})
 # data.corr()
 
-def load_spam_dataset(load_train_labels: bool = False):
+def load_dataset(load_train_labels: bool = False):
 
     df = pd.read_csv('B:\\Desktop\\Studium Gruppenarbeiten\\LabellingWithSnorkel\\weatherAUS.csv')
     df= df.dropna()
@@ -24,12 +24,11 @@ def load_spam_dataset(load_train_labels: bool = False):
     # Shuffle order
     df = df.sample(frac=1, random_state=123).reset_index(drop=True)
 
-    
     df_train =df
 
     if not load_train_labels:
         df_train, df_test = train_test_split(df_train, test_size=1250, random_state=123, stratify=df_train.label)
         df_train["label"] = np.ones(len(df_train["label"])) * -1
-
-
+        #df_train.loc[:,"label"]= -1
+        
     return df_train, df_test, df
